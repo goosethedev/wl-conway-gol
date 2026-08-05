a9c7aac318ff384f7aafa05349daa5dbc896f1f6
Gus Sánchez <goosethedev@proton.me>
2026-08-05 16:18:41 -0400

    updated to wayland-rs 0.31.15

--- src/bin/wl-client-game.rs
+++ src/bin/wl-client-game.rs
@@ -3,6 +3,7 @@ use std::{os::fd::AsFd, time::Duration};
 use wl_conway::GameOfLife;
 
 use calloop::timer::{TimeoutAction, Timer};
+use calloop_wayland_source::WaylandSource;
 use wayland_client::{
     Connection, Dispatch, QueueHandle, delegate_noop,
     globals::{BindError, GlobalList, GlobalListContents, registry_queue_init},
@@ -27,8 +28,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut event_loop = calloop::EventLoop::<AppState>::try_new()?;
 
     // Insert the event_queue into the loop to handle server events
-    let wl_source = calloop_wayland_source::WaylandSource::new(conn, event_queue);
-    wl_source.insert(event_loop.handle())?;
+    WaylandSource::new(conn, event_queue).insert(event_loop.handle())?;
 
     // Setup timer source for updating the UI periodically
     let timer = Timer::from_duration(Duration::from_millis(TICK_MILLIS));
@@ -81,15 +81,15 @@ struct AppState {
 impl AppState {
     fn new(globals: &GlobalList, qh: &QueueHandle<Self>) -> Result<Self, BindError> {
         // Obtain compositor object and create a wl_surface
-        let compositor: wl_compositor::WlCompositor = globals.bind(&qh, 1..=6, ())?;
+        let compositor: wl_compositor::WlCompositor = globals.bind(qh, 1..=6, ())?;
         let wl_surface = compositor.create_surface(qh, ());
 
         // Get an xdg_surface from the wl_surface
-        let xdg_wm_base: xdg_wm_base::XdgWmBase = globals.bind(&qh, 1..=6, ())?;
+        let xdg_wm_base: xdg_wm_base::XdgWmBase = globals.bind(qh, 1..=6, ())?;
         let xdg_surface = xdg_wm_base.get_xdg_surface(&wl_surface, qh, ());
 
         // Get a toplevel (regular window) from the xdg_surface
-        let xdg_toplevel = xdg_surface.get_toplevel(&qh, ());
+        let xdg_toplevel = xdg_surface.get_toplevel(qh, ());
         xdg_toplevel.set_app_id("game_of_life".to_string());
         xdg_toplevel.set_title("Conway's Game of Life".to_string());
 
@@ -153,9 +153,9 @@ impl AppState {
         drop(buf);
 
         // Create the SHM pool and a buffer from it
-        let pool = self.wl_shm.create_pool(buf_file.as_fd(), width * height * 4, &qh, ());
-        let wl_buffer =
-            pool.create_buffer(0, width, height, width * 4, wl_shm::Format::Xrgb8888, &qh, ());
+        let format = wl_shm::Format::Xrgb8888;
+        let pool = self.wl_shm.create_pool(buf_file.as_fd(), width * height * 4, qh, ());
+        let wl_buffer = pool.create_buffer(0, width, height, width * 4, format, qh, ());
 
         // Attach the buffer and trigger a re-render
         self.wl_surface.attach(Some(&wl_buffer), 0, 0);
@@ -178,12 +178,12 @@ impl AppState {
 
 impl Dispatch<wl_registry::WlRegistry, GlobalListContents> for AppState {
     fn event(
-        _state: &mut Self,
+        _state: &mut AppState,
         _registry: &wl_registry::WlRegistry,
         _event: <wl_registry::WlRegistry as wayland_client::Proxy>::Event,
         _data: &GlobalListContents,
         _conn: &Connection,
-        _qhandle: &wayland_client::QueueHandle<Self>,
+        _qhandle: &QueueHandle<AppState>,
     ) {
         // TODO: Deal with dynamic globals e.g. plugged/unplugged monitors, input, etc.
     }
@@ -191,12 +191,12 @@ impl Dispatch<wl_registry::WlRegistry, GlobalListContents> for AppState {
 
 impl Dispatch<xdg_wm_base::XdgWmBase, ()> for AppState {
     fn event(
-        _state: &mut Self,
+        _state: &mut AppState,
         proxy: &xdg_wm_base::XdgWmBase,
         event: <xdg_wm_base::XdgWmBase as wayland_client::Proxy>::Event,
         _data: &(),
         _conn: &Connection,
-        _qhandle: &wayland_client::QueueHandle<Self>,
+        _qhandle: &QueueHandle<AppState>,
     ) {
         // Ping handling
         if let xdg_wm_base::Event::Ping { serial } = event {
@@ -207,12 +207,12 @@ impl Dispatch<xdg_wm_base::XdgWmBase, ()> for AppState {
 
 impl Dispatch<xdg_surface::XdgSurface, ()> for AppState {
     fn event(
-        state: &mut Self,
+        state: &mut AppState,
         proxy: &xdg_surface::XdgSurface,
         event: <xdg_surface::XdgSurface as wayland_client::Proxy>::Event,
         _data: &(),
         _conn: &Connection,
-        qh: &QueueHandle<Self>,
+        qh: &QueueHandle<AppState>,
     ) {
         // Handle the end of XdgToplevel Configure sequences, rendering the game
         if let xdg_surface::Event::Configure { serial } = event {
@@ -225,12 +225,12 @@ impl Dispatch<xdg_surface::XdgSurface, ()> for AppState {
 
 impl Dispatch<xdg_toplevel::XdgToplevel, ()> for AppState {
     fn event(
-        state: &mut Self,
+        state: &mut AppState,
         _proxy: &xdg_toplevel::XdgToplevel,
         event: <xdg_toplevel::XdgToplevel as wayland_client::Proxy>::Event,
         _data: &(),
         _conn: &Connection,
-        _qhandle: &wayland_client::QueueHandle<Self>,
+        _qh: &QueueHandle<AppState>,
     ) {
         // TODO: Handle Configure events (resizes, fullscreen, etc)
         // Note: do NOT render here yet, only apply changes to internal state
@@ -243,19 +243,19 @@ impl Dispatch<xdg_toplevel::XdgToplevel, ()> for AppState {
 
 impl Dispatch<wl_callback::WlCallback, ()> for AppState {
     fn event(
-        app: &mut Self,
-        _callback: &wl_callback::WlCallback,
-        event: wl_callback::Event,
-        _: &(),
-        _: &Connection,
-        qh: &QueueHandle<Self>,
+        state: &mut AppState,
+        _proxy: &wl_callback::WlCallback,
+        event: <wl_callback::WlCallback as wayland_client::Proxy>::Event,
+        _data: &(),
+        _conn: &Connection,
+        qh: &QueueHandle<AppState>,
     ) {
         // The compositor signalled it's ok to send a new frame (response to wl_surface.frame)
         // so we render a new one
         if let wl_callback::Event::Done { .. } = event {
-            app.frame_pending = false;
-            app.render_grid(qh);
-            app.needs_update = false;
+            state.frame_pending = false;
+            state.render_grid(qh);
+            state.needs_update = false;
         }
     }
 }
